623f77ed3ce5e1d7589fa629e155f75845c165c2
icyzeroice <ziv3@outlook.com>
2020-05-16 20:59:19 +0800

    improvement (Getting-Started) : improve main.cpp

--- 0-Getting-Started/main.cpp
+++ 0-Getting-Started/main.cpp
@@ -3,14 +3,25 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
 
 
 int main(int argc, char** argv)
 {
+    // (0, 0) is at the left-top
     TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
+
+    image.set(0, 10, red);
+    image.set(0, 11, red);
+
     image.flip_vertically();
+
+    image.set(0, 10, blue);
+    image.set(0, 11, blue);
+
     image.write_tga_file("output.tga");
+
     return 0;
 }
 
