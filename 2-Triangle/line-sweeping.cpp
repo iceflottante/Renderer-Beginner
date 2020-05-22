a7ac684f6a1f7ae0e54c511c5280ad37cd4fe349
icyzeroice <ziv3@outlook.com>
2020-05-23 00:05:45 +0800

    initialize (Triangle)

---
+++ 2-Triangle/line-sweeping.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../Lib/tgaimage.h"
+
+
+void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color)
+{
+    
+}
+
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
+
+// file ends here
+
