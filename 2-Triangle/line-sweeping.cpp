ee79372cf333f7076587968ab1bc171231b0a3d9
icyzeroice <ziv3@outlook.com>
2020-05-23 16:42:44 +0800

    add (Triangle) : outline

--- 2-Triangle/line-sweeping.cpp
+++ 2-Triangle/line-sweeping.cpp
@@ -1,15 +1,30 @@
-#include <iostream>
-#include "../Lib/tgaimage.h"
+﻿#include <iostream>
+#include "../1-Line/Lib/model.h"
+#include "line.h"
 
 
-void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color)
+void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor &color)
 {
-    
+    line(v0.x, v0.y, v1.x, v1.y, image, color);
+    line(v1.x, v1.y, v2.x, v2.y, image, color);
+    line(v2.x, v2.y, v0.x, v0.y, image, color);
 }
 
 
 int main(int argc, char** argv)
 {
+    TGAImage image(200, 200, TGAImage::RGB);
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+
+    triangle(t0[0], t0[1], t0[2], image, red); 
+    triangle(t1[0], t1[1], t1[2], image, white); 
+    triangle(t2[0], t2[1], t2[2], image, green);
+
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
 
     return 0;
 }
---
+++ 2-Triangle/line.cpp
@@ -0,0 +1,51 @@
+#include "../Lib/tgaimage.h"
+
+
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor &color)
+{
+    int dx = std::abs(x0 - x1);
+    int dy = std::abs(y0 - y1);
+    bool needTranspose = false;
+
+    if (dx < dy)
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        std::swap(dx, dy);
+        needTranspose = true;
+    }
+
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int derror = std::abs(dy) * 2;
+    int error = 0;
+    int y = y0;
+
+    for (int x = x0; x <= x1; x++) {
+
+        if (needTranspose)
+        {
+            image.set(y, x, color); 
+        }
+        else
+        {
+            image.set(x, y, color); 
+        }
+
+        error += derror;
+
+        if (error > dx)
+        {
+            y += y1 > y0 ? 1 : -1;
+            error -= dx * 2;
+        }
+    } 
+}
+
+// file ends here
+
